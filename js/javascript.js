2a03fe501a5499e8f8de95f7159298ef8253dced
Brianna Greene <brianna.greene.designs@gmail.com>
2017-08-11 15:04:50 -0600

    taking a break to do stop light project

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,46 @@
-// wrtie a function to create divs to fill the space
-console.log("jquery is ready")
+console.log("javascript is ready")
+// JQUERY FUNCTION TO CREATE PIXEL DIVS
+// $(document).ready(function(){
+//   createGrid(1174)
+// })
+//
+// function createGrid(number) {
+//  for (var i = 0; i <= number; i++) {
+//   $('.wrapper').append('<div class="pixel"></div>')
+//  }
+//
+//  $('.pixel').click(function(){
+//    console.log("click function is hooked up!")
+//  })
+// }
+//
+// $('.pixel').hover(function(){
+//   $(this).css
+// })
 
-$(document).ready(function(){
-  createGrid(1174)
-})
 
+// JAVASCRIPT VERSION TO CREATE PIXEL DIVS
 function createGrid(number) {
- for (var i = 0; i <= number; i++) {
-  $('.wrapper').append('<div class="pixel"></div>')
- }
- // $('.pixel').height($('.wrapper').height()/number)
- // $('.pixel').width($('.pixel').width()/number)
- $('.pixel').click(function(){
-   console.log("click function is hooked up!")
- })
+  let wrapper = document.querySelector('.wrapper')
+
+  for (var i = 0; i <= number; i++) {
+    let div = document.createElement('div')
+    div.className = 'pixel'
+    wrapper.append(div)
+  }
 }
 
-$('.pixel').hover(function(){
-  $(this).css
+// FUNCTION TO GRAB BACKGROUND COLOR ON CLICK
+// SET BACKGROUND COLOR TO CLICKED PIXEL
+function clickEvent() {
+  let colorDiv = document.getElementById('one')
+}
+
+// EVENT LISTENER?
+document.addEventListener("click", function() {
+  //list functions here?
 })
+
+
+// -this (element of html doc)
+// -element being clicked or whatever (event.target)
